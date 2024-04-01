0865b625807dc7b3ee246e8262721a7dfd28f8e4
NahsJ90 <nashj6@live.it>
2024-04-01 18:31:22 +0200

    section 6 codes

--- routes/admin.js
+++ routes/admin.js
@@ -6,7 +6,7 @@ const path = require("path");
 
 const router = express.Router();
 
-const adminProducts = []; // this array will store the data from the form
+const adminProducts = [];
 
 router.use('/add-product/new-product', (req, res, next) => {
     res.send('<h1>This is the product page</h1>');
@@ -19,7 +19,6 @@ router.post("/add-product", (req, res, next) => {
 
 router.get('/add-product', (req, res, next) => {
     res.render('add-product', { pageTitle: 'Add Product', path: '/admin/add-product' });
-    //res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 });
 
-module.exports = { router, adminProducts }; // Since the export now is an object, we have to modify how import it inside the app.js file
+module.exports = { router, adminProducts };
--- routes/shop.js
+++ routes/shop.js
@@ -8,11 +8,6 @@ const path = require("path");
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-    // The limit of this type of data sharing is that all the clients will see all the data  and there will be not differentiation. 
-    // Most of the time we want each customer to see a specific type of data. The Database will let us do so. 
-    //console.log(products);
-
-    // res.sendFile(path.join(rootDir, 'views', 'shop.html'));
     res.render('shop', { products: adminProducts, pageTitle: 'Shop', path: '/' });
 });
 
